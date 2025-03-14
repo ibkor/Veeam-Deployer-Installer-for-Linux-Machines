2e8e94c1e256947d50704f6557b27b78394991a5
ibkor <ibrahim-korkusuz@hotmail.com>
2025-03-14 10:05:18 +0300

    Update VeeamDeployerFinal.ps1

--- VeeamDeployerFinal.ps1
+++ VeeamDeployerFinal.ps1
@@ -1,4 +1,4 @@
-﻿$ExportPath = "C:\temp"   #Path to save deployment kit on VBR Server
+$ExportPath = "C:\temp"   #Path to save deployment kit on VBR Server
 $csvFilePath = "C:\csv\hostnames.csv" #Where we keep linux hostnames, Prepare CSV content with hostnames only. First line: Hostname, other lines: 1 hostname on each line
 $TargetPath = "/tmp/VeeamUpload"  #Path on Linux Machines, where the files will be copied
 $LogFilePath = "C:\logs\installation_log.txt" # Path to log file on VBR Server
@@ -8,24 +8,35 @@ Clear-Content -Path $LogFilePath -ErrorAction SilentlyContinue
 
 $csvData = Import-Csv -Path $csvFilePath | Select-Object -ExpandProperty Hostname
 
-Generate-VBRBackupServerDeployerKit -ExportPath $ExportPath
+Generate-VBRBackupServerDeployerKit -ExportPath $ExportPath -ValidityPeriodInHours 48
 
 $detectDistro = 'source /etc/os-release && echo $ID'
 
 foreach ($hostname in $csvData) {
     $sshUser = "$username@$hostname"
-    $Destination = "$username@$hostname"":$TargetPath"
-    $initialmessage = "Deployment Kit is being transferred to $hostname at $TargetPath`n"
-
+    $Destination = "$username@$hostname"":$TargetPath"  
     Add-Content -Path $LogFilePath -Value "$separatorLine"
-    Add-Content -Path $LogFilePath -Value "$separatorLine"
-    Write-Host $initialmessage
-    Add-Content -Path $LogFilePath -Value $initialmessage
 
     try {
-        cd "C:\Users\Administrator\Desktop\OpenSSH-Win64"
+        # Check if the Veeam nosnap package is installed
+        Write-Host "Verifying veeam-nosnap installation"
+        $checknoSnap = "rpm -qa | grep veeam"
+        $noSnapStatus = ssh $sshUser "$checknoSnap" 2>&1
+        Write-Host $noSnapStatus
 
-        $scpOutput = (& { ./scp -r -v -o Ciphers=aes256-ctr -o MACs=hmac-sha2-256 "$ExportPath/*" $Destination 2>&1 }) | Tee-Object -Variable scpLog
+        if ($noSnapStatus -like "*nosnap*") {
+        $nosnapmessage = "veeam-nosnap installation detected, skipping installation on host: $hostname"
+        Write-Host $nosnapmessage
+        Add-Content -Path $LogFilePath -Value $nosnapmessage
+        Continue
+        }
+
+        $initialmessage = "Deployment Kit is being transferred to $hostname at $TargetPath`n"
+        Write-Host $initialmessage
+        Add-Content -Path $LogFilePath -Value $initialmessage
+        Add-Content -Path $LogFilePath -Value "$separatorLine"
+    
+        $scpOutput = (& { scp -r -v -o Ciphers=aes256-ctr -o MACs=hmac-sha2-256 "$ExportPath/*" $Destination 2>&1 }) | Tee-Object -Variable scpLog
         
         if ($scpOutput -like "*failed to upload file*" -or $scpOutput -like "*Permission denied*") {
         Write-Host "Transfer failed. Check installation_logs for more information."
@@ -40,7 +51,7 @@ foreach ($hostname in $csvData) {
         Add-Content -Path $LogFilePath -Value $successMessage
 
         # Detect the distribution and choose package manager command
-        $distroOutput = ./ssh -q -o Ciphers=aes256-ctr -o MACs=hmac-sha2-256 $sshUser "$detectDistro" 2>&1 
+        $distroOutput = ssh -q -o Ciphers=aes256-ctr -o MACs=hmac-sha2-256 $sshUser "$detectDistro" 2>&1         
         $distroType = $distroOutput.Trim()
 
         Write-Host "Detected distro: $distroOutput"
@@ -62,7 +73,7 @@ foreach ($hostname in $csvData) {
             "sudo /opt/veeam/deployment/veeamdeploymentsvc --install-certificate client-cert.pem; " +
             "sudo /opt/veeam/deployment/veeamdeploymentsvc --restart"
 
-        $output = ./ssh -q -o Ciphers=aes256-ctr -o MACs=hmac-sha2-256 $sshUser "bash -c '$commands'" 2>&1 
+        $output = ssh -q -t -o Ciphers=aes256-ctr -o MACs=hmac-sha2-256 $sshUser "bash -c '$commands'" 2>&1 
 
         $separatorLine = "=" * 50 
         Add-Content -Path $LogFilePath -Value "$separatorLine"
@@ -74,18 +85,22 @@ foreach ($hostname in $csvData) {
 
         Add-Content -Path $LogFilePath -Value "$separatorLine"
 
-        # Check for errors in the output before deciding on success
-        if ($output -like "*Permission denied*" -or $output -like "*a terminal is required*" -or $output -like "*error*") {
-            $errorMessage = "Installation on $hostname failed due to permission issues or sudo configuration`n"
-            Write-Host $errorMessage  
-            Add-Content -Path $LogFilePath -Value $errorMessage
-            Read-Host -Prompt "Press Enter to continue working on the next machine"
-            Continue
+# Check for errors in the output before deciding on success
+        $checkPackageCommand = "rpm -q veeamdeployment-12.2.0.334-1.x86_64"
+        $packageStatus = ssh $sshUser "$checkPackageCommand" 2>&1
+      
+        if ($packageStatus -like "veeamdeployment-12.2.0.334-1.x86_64*") {
+        $successfulHosts += $hostname
+        $successMessage = "$hostname Package is successfully installed."
+        Write-Host $successMessage
+        Add-Content -Path $LogFilePath -Value $successMessage
         } 
-          
-        $installSuccessMessage = "Installation on $hostname succeeded`n"
-        Write-Host $installSuccessMessage 
-        Add-Content -Path $LogFilePath -Value $installSuccessMessage
+        else {
+        $unsuccessfulHosts += $hostname
+        $errorMessage = "$hostname Package is not installed, or password is incorrect. Status: $packageStatus"
+        Write-Host $errorMessage
+        Add-Content -Path $LogFilePath -Value $errorMessage
+        }
 
     } catch {
         $errorMessage = "Error encountered on $hostname"
@@ -96,4 +111,4 @@ foreach ($hostname in $csvData) {
     Read-Host -Prompt "Press Enter to continue working on the next machine"
 }
 
-Read-Host -Prompt "Press Enter to exit"
+Read-Host -Prompt "Press Enter to exit" 
